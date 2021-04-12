ecc6a51ccbd117d1e4e5d88f632c7289e938c458
Aleksey <luksiko90@gmail.com>
2021-04-12 08:52:59 +0500

    Day-4 adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 5,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6,
+        },
+        1600: {
+            slidesPerView: 5,
+        },
+        1300: {
+            slidesPerView: 4,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 2,
+        }
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.channel-button-next',
@@ -12,7 +30,16 @@ const swiper = new Swiper('.channel-slider', {
 const swiper2 = new Swiper('.recommended-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1500: {
+            slidesPerView: 3,
+        },
+        1100: {
+            slidesPerView: 2,
+        },
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.recommended-button-next',
@@ -22,10 +49,40 @@ const swiper2 = new Swiper('.recommended-slider', {
 const swiperFood = new Swiper('.food-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 5,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1900: {
+            slidesPerView: 6,
+        },
+        1600: {
+            slidesPerView: 5,
+        },
+        1300: {
+            slidesPerView: 4,
+        },
+        1100: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 2,
+        }
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.food-button-next',
         prevEl: '.food-button-prev',
     },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () =>{
+    mobileSearch.classList.toggle('is-open')
+})
+
+if (document.documentElement.scrollWidth<=640){
+    swiperFood.destroy()
+    swiper2.destroy()
+    swiper.destroy()
+}
